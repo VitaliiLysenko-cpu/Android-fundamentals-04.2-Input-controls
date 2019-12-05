859606874c5425a9bf708a9b4dc4c1d5275a6ec5
italiy <vit47903@gmail.com>
2019-12-05 22:12:32 +0200

    code formatted, change logic of creation message, delete switch-case in onClick method

--- app/src/main/java/com/example/inputcontrols042homework/MainActivity.java
+++ app/src/main/java/com/example/inputcontrols042homework/MainActivity.java
@@ -20,31 +20,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         showToast = findViewById(R.id.show_toast_Button);
         showToast.setOnClickListener(this);
-        checkBoxes = new CheckBox[]{findViewById(R.id.chocolate_syrup_checkBox),
+        checkBoxes = new CheckBox[]{
+                findViewById(R.id.chocolate_syrup_checkBox),
                 findViewById(R.id.sprinkles_checkBox),
                 findViewById(R.id.crushed_nuts_checkBox),
                 findViewById(R.id.cremes_checkBox),
-                findViewById(R.id.orio_cookie_crumbles_checkBox)};
+                findViewById(R.id.orio_cookie_crumbles_checkBox)
+        };
     }
 
     public String getToastMessage() {
         String message = "Toppings: ";
+        String control = null;
         for (CheckBox checkBox : checkBoxes) {
             if (checkBox.isChecked()) {
-                message = message.concat(checkBox.getText().toString() + ", ");
+                if (control == null) {
+                    message = message.concat(checkBox.getText().toString());
+                    control = message.concat(checkBox.getText().toString());
+                } else {
+                    message = message.concat(", " + checkBox.getText().toString());
+                }
             }
         }
-        //  message = message.substring(0 , message.lastIndexOf(","));
-        return message.substring(0, message.lastIndexOf(","));
+        return message;
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.show_toast_Button:
-                displayToast(getToastMessage());
-                break;
-        }
+        displayToast(getToastMessage());
     }
 
     public void displayToast(String message) {
